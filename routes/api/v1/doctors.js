c5c0f4b9c857c5b6efcd783816d240f65107b666
Pushpendra Singh <pushpendra7060@gmail.com>
2020-08-03 14:46:54 +0530

    doctor api is completed

--- routes/api/v1/doctors.js
+++ routes/api/v1/doctors.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-router.use('/register' , require('../../../controllers/api/v1/doctors_api').register);
-router.use('/login' , require('../../../controllers/api/v1/doctors_api').login);
-
-
+router.use(
+  "/register",
+  require("../../../controllers/api/v1/doctors_api").register
+);
+router.use("/login", require("../../../controllers/api/v1/doctors_api").login);
 
 module.exports = router;
