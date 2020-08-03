da4639f184ed8d16243455c757208aef64d2b942
Pushpendra Singh <pushpendra7060@gmail.com>
2020-08-03 14:49:04 +0530

    patiant resister is completed

--- routes/api/v1/patients.js
+++ routes/api/v1/patients.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 
-router.post('/register',passport.authenticate('jwt',{session:false}),require('../../../controllers/api/v1/patient_api').register);
-router.post('/:id/create_report',passport.authenticate('jwt',{session:false}) ,require('../../../controllers/api/v1/patient_api').createReport);
-router.post('/:id/all_reports', require('../../../controllers/api/v1/patient_api').allreports);
+router.post(
+  "/register",
+  passport.authenticate("jwt", { session: false }),
+  require("../../../controllers/api/v1/patient_api").register
+);
+router.post(
+  "/:id/create_report",
+  passport.authenticate("jwt", { session: false }),
+  require("../../../controllers/api/v1/patient_api").createReport
+);
+router.post(
+  "/:id/all_reports",
+  require("../../../controllers/api/v1/patient_api").allreports
+);
 module.exports = router;
